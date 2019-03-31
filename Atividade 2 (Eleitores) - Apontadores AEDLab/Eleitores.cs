4cd74117917deb75e8cdfb8dc873f08318072e97
Gustavo Sena <gustavosena98@LIVE.COM>
2019-03-31 12:14:28 -0300

    Implementação método Equals por sexo e por seção e Zona eleitoral

--- Atividade 2 (Eleitores) - Apontadores AEDLab/Eleitores.cs
+++ Atividade 2 (Eleitores) - Apontadores AEDLab/Eleitores.cs
@@ -8,5 +8,29 @@ namespace Atividade_2__Eleitores____Apontadores_AEDLab
 {
     class Eleitores //Gustavo
     {
+        public string Nome { get; private set; }
+        public char Sexo { get; private set; }
+        public string Titulo_Eleitor { get; private set; }
+        public int Zona_Eleitoral { get; private set; }
+        public int Secao_Eleitoral { get; private set; }
+
+        public Eleitores(string Nome, char Sexo, string Titulo_Eleitor, int Zona_Eleitoral, int Secao_Eleitoral)
+        {
+            this.Nome = Nome;
+            this.Sexo = Sexo;
+            this.Titulo_Eleitor = Titulo_Eleitor;
+            this.Zona_Eleitoral = Zona_Eleitoral;
+            this.Secao_Eleitoral = Secao_Eleitoral;
+        }
+
+        public bool Equals(char sexo)
+        {
+            return (this.Sexo == sexo);
+        }
+
+        public bool Equals(int Zona_Eleitoral, int Secao_Eleitoral)
+        {
+            return (this.Zona_Eleitoral == Zona_Eleitoral && this.Secao_Eleitoral == Secao_Eleitoral);
+        }
     }
 }
