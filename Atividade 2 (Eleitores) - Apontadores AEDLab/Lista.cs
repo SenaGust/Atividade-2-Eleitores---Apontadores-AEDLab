aac4820bf069b220ed8ebf09e5b60e1a1cbe933e
Gustavo Sena <gustavosena98@LIVE.COM>
2019-03-31 12:15:15 -0300

    Criação de todos os métodos e suas orientações

--- Atividade 2 (Eleitores) - Apontadores AEDLab/Lista.cs
+++ Atividade 2 (Eleitores) - Apontadores AEDLab/Lista.cs
@@ -6,7 +6,50 @@ using System.Threading.Tasks;
 
 namespace Atividade_2__Eleitores____Apontadores_AEDLab
 {
-    class Lista //Lorena
+    class Lista
     {
+        public Lista() //Lorena
+        {
+
+        }
+
+        public void Inserir(Eleitores dado) //Lorena
+        {
+            // insira elementos ao final da lista
+
+        }
+
+        public Eleitores Retirar(string Titulo) //Gustavo
+        {
+            // Este método deve receber como
+            // parâmetro o título da pessoa que deve ser excluída;
+
+            return null;
+        }
+
+        public int CarregarDados (string nomeArquivo) //Lorena
+        {
+            // Este método deve receber como parâmetro o nome de um arquivo texto e
+            // carregar (ler) os dados constantes deste arquivo para a lista. O retorno do método é a
+            // quantidade de registros lidos.
+            
+            return -1;
+        }
+
+        public Lista EleitoresPorSecao(int zona, int secao) //Lorena
+        {
+            // localiza, na lista de eleitores, aqueles que pertençam à zona e seção especificados. 
+            // Este método retorna uma lista com os eleitores encontrados.
+
+            return null;
+        }   
+
+        public Lista EleitoresPorSexo(char sexo) //Gustavo
+        {
+            // Este método recebe como parâmetro um caractere indicando o sexo(masculino ou feminino) e retorna a lista de eleitores
+            // formada exclusivamente por pessoas do sexo informado.
+
+            return null;
+        }
     }
 }
