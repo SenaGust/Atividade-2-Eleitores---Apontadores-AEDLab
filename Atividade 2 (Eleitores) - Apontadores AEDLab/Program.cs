801e2c7968d678822d4b044a79a5f3facdf798e1
loriAguilar <44687575+loriAguilar@users.noreply.github.com>
2019-03-30 23:46:27 -0300

    Organização do algoritmo

--- Atividade 2 (Eleitores) - Apontadores AEDLab/Program.cs
+++ Atividade 2 (Eleitores) - Apontadores AEDLab/Program.cs
@@ -6,11 +6,48 @@ using System.Threading.Tasks;
 
 namespace Atividade_2__Eleitores____Apontadores_AEDLab
 {
+    /*
+     *1. Implementar classe Lista
+     *2. Implementar classe Eleitores
+     *3. Implementar método LeituraArquivo()
+     *4. Implementar método EliminarEleitor()
+     *5. Implementar método LocalizaPorSecao()
+     *6. Implementar método LocalizaPorSexo()
+     */
     class Program
     {
         static void Main(string[] args)
         {
 
         }
+
+        #region Leitura_de_arquivos (Lorena)
+        static void Leitura_de_arquivos() 
+        {
+
+        }
+        #endregion
+
+        #region Eliminar_Eleitor (Gustavo)
+        static void Eliminar_Eleitor()
+        {
+
+        }
+        #endregion
+
+        #region Localizar_por_seção (Lorena)
+        static void Localizar_por_seção()
+        {
+
+        }
+        #endregion
+
+        #region Localizar_por_sexo (Gustavo)
+        static void Localizar_por_sexo()
+        {
+
+        }
+        #endregion 
+
     }
 }
