d9a44b90f4eb595c974802fb2cb581da9a38672f
Gustavo Sena <gustavosena98@LIVE.COM>
2019-04-01 08:01:46 -0300

    Implementação do código de teste

--- Atividade 2 (Eleitores) - Apontadores AEDLab/Program.cs
+++ Atividade 2 (Eleitores) - Apontadores AEDLab/Program.cs
@@ -7,47 +7,80 @@ using System.IO;
 
 namespace Atividade_2__Eleitores____Apontadores_AEDLab
 {
-    /*
-     *1. Implementar classe Lista
-     *2. Implementar classe Eleitores
-     *3. Implementar método LeituraArquivo()
-     *4. Implementar método EliminarEleitor()
-     *5. Implementar método LocalizaPorSecao()
-     *6. Implementar método LocalizaPorSexo()
-     */
     class Program
     {
         static void Main(string[] args)
         {
-            Lista list = new Lista();
+            Lista ListaEleitores = new Lista();
+
+            #region Testando Carregar Dados
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+            Console.WriteLine("\tTestando Carregar Dados");
+            
             string path = "eleitores.txt";
-            list.CarregarDados(path);
-            Console.WriteLine("Teste"+list.);
+            Console.WriteLine("Nome do arquivo: {0}", path);
+            ListaEleitores.CarregarDados(path);
+
+            Console.WriteLine(ListaEleitores.ToString());
+            #endregion
+
+            #region Testando Inserir
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine("\tTestando Inserir");
+
+            ListaEleitores.Inserir(new Eleitores("Gustavo Sena", 'm', "52484246247", 5, 4));
+            ListaEleitores.Inserir(new Eleitores("Lorena Aguilar", 'f', "86444456548", 2, 3));
+            ListaEleitores.Inserir(new Eleitores("Leonardo Souxa", 'm', "13155484555", 5, 4));
+            ListaEleitores.Inserir(new Eleitores("Eu não sei", 'm', "89456514879", 2, 3));
+            ListaEleitores.Inserir(new Eleitores("olá, mundo", 'm', "52484246247", 5, 4));
+            ListaEleitores.Inserir(new Eleitores("Luiza Polewacz", 'f', "65148454954", 2, 3));
+            ListaEleitores.Inserir(new Eleitores("Bruna Vitória", 'f', "84645648855", 5, 4));
+
+            Console.WriteLine(ListaEleitores.ToString());
+            #endregion
+
+            #region Testando criar sublistas
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("\tTestando SubListas");
+            Console.WriteLine("\tMulheres");
+            Console.WriteLine(ListaEleitores.EleitoresPorSexo('f').ToString());
+
+            Console.WriteLine("\tHomens");
+            Console.WriteLine(ListaEleitores.EleitoresPorSexo('m').ToString());
+
+            Console.WriteLine("\tZona: 2\t Seção: 3");
+            Console.WriteLine(ListaEleitores.EleitoresPorSecao(2,3).ToString());
+
+            Console.WriteLine("\tZona: 5\t Seção: 4");
+            Console.WriteLine(ListaEleitores.EleitoresPorSecao(5, 4).ToString());
+            #endregion
+
+            #region Testando Retirar
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+
+            Console.WriteLine("\tTestando Retirar");
+
+            Console.WriteLine("Após retirar o Titulo: 20101112394");
+            ListaEleitores.Retirar("20101112394");
+            Console.WriteLine(ListaEleitores.ToString());
+
+            Console.WriteLine("Após retirar o Titulo: 10080870655");
+            ListaEleitores.Retirar("10080870655");
+            Console.WriteLine(ListaEleitores.ToString());
+            #endregion
+
+            //Lista list = new Lista();
+            //string path = "eleitores.txt";
+            //list.CarregarDados(path);
+            //Console.WriteLine(list.ToString());
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("\n\nPressione qualquer tecla para continuar...");
             Console.ReadKey();
         }
-
-        
-
-        #region Eliminar_Eleitor (Gustavo)
-        static void Eliminar_Eleitor()
-        {
-
-        }
-        #endregion
-
-        #region Localizar_por_seção (Lorena)
-        static void Localizar_por_seção()
-        {
-
-        }
-        #endregion
-
-        #region Localizar_por_sexo (Gustavo)
-        static void Localizar_por_sexo()
-        {
-
-        }
-        #endregion 
-
     }
 }
